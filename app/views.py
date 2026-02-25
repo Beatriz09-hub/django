ccf3df758ee33ec3fd90a379965c4c7238828985
beatrizlimeira70@gmail.com <Beatriz09hub>
2026-02-25 20:49:51 -0300

    criando app e views

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h1>Hello World!</h1>')
+
+def outra(request):
+    return HttpResponse('<h1>Outra página</h1>')
+
+
